3489bc1f02eedb2ecca15d3e16279f02127dd22e
PGijsbers <p.gijsbers@tue.nl>
2019-03-21 14:57:58 +0200

    Add function to create a pipeline that is (hopefully) similar to initial setup.

---
+++ utils.py
@@ -0,0 +1,36 @@
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.feature_selection import VarianceThreshold
+
+__version__ = '0.0'
+
+
+class SimpleImputerDuplicate(SimpleImputer):
+    pass
+
+
+def simple_construct_pipeline_for_task(task, learner):
+    cat_indices = task.get_dataset().get_features_by_type('nominal', [task.target_name])
+    num_indices = task.get_dataset().get_features_by_type('numeric', [task.target_name])
+
+    return Pipeline(steps=
+                    [
+                        ("impute", ColumnTransformer(
+                            transformers=[
+                                ("nominal", SimpleImputer(strategy="most_frequent"), cat_indices),
+                                ("numeric", SimpleImputerDuplicate(strategy="mean"), num_indices)
+                            ],
+                            remainder="passthrough"
+                        )),
+                        ("transform", ColumnTransformer(
+                            transformers=[
+                                ("nominal", OneHotEncoder(categories='auto'), [*range(len(cat_indices))]),
+                                ("numeric", StandardScaler(), [*range(len(cat_indices), len(cat_indices) + len(num_indices))])
+                            ],
+                            remainder="passthrough"
+                        )),
+                        ("feature_selection", VarianceThreshold()),
+                        ("clasification", learner)
+                    ])
