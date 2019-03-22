eb7f5cf3bd0009f44604ed1c069f9347e260fc08
PGijsbers <p.gijsbers@tue.nl>
2019-03-22 10:46:36 +0200

    Work-around for CSR formatted datasets.

--- surrogates/metafeatures.py
+++ surrogates/metafeatures.py
@@ -1,6 +1,7 @@
 import random
 import statistics
 
+import scipy.sparse
 from sklearn.preprocessing import scale
 import numpy as np
 import pandas as pd
@@ -12,12 +13,15 @@ from utils import simple_construct_pipeline_for_task
 def create_metadataset(task_ids, after_preprocessing=True):
     metafeatures = {}
     for task_id in task_ids:
-        task = openml.tasks.get_task(task_id)
-        if after_preprocessing:
-            pipeline = simple_construct_pipeline_for_task(task)
-            metafeatures[task_id] = calculate_metafeatures(task, preprocessing=pipeline)
-        else:
-            metafeatures[task_id] = calculate_metafeatures(task, preprocessing=None)
+        try:
+            task = openml.tasks.get_task(task_id)
+            if after_preprocessing:
+                pipeline = simple_construct_pipeline_for_task(task)
+                metafeatures[task_id] = calculate_metafeatures(task, preprocessing=pipeline)
+            else:
+                metafeatures[task_id] = calculate_metafeatures(task, preprocessing=None)
+        except Exception as e:
+            print('Skipped task {} because of {}'.format(task_id, str(e)))
 
     return pd.DataFrame.from_dict(metafeatures, orient='index')
 
@@ -27,10 +31,15 @@ def calculate_metafeatures(task, preprocessing=None):
     if preprocessing is not None:
         X = preprocessing.fit_transform(X)
 
+    if isinstance(X, scipy.sparse.csr_matrix):
+        # quick hack for now as code below assumes operations that cant be done on csr matrix (most notably mkd code)
+        X = X.todense().A
+
     classes, counts = np.unique(y, return_counts=True)
     if preprocessing is not None:
-        # For now assume preprocessing always include one-hot encoding.
-        n_categorical = len([x for x in X.T if len(set(x)) == 2])
+        # For now assume preprocessing always include one-hot encoding and does not introduce additional columns
+        # for numeric transformations. Might be off if constant features are removed.
+        n_categorical = X.shape[1] - len(task.get_dataset().get_features_by_type('numeric', [task.target_name]))
     else:
         n_categorical = len(task.get_dataset().get_features_by_type('nominal', [task.target_name]))
 
