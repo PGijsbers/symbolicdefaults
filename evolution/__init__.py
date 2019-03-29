c7a922deea541202c4a11171ba40a64d7d7519ee
PGijsbers <p.gijsbers@tue.nl>
2019-03-29 13:32:50 +0200

    Add operator for svc_kernel

--- evolution/__init__.py
+++ evolution/__init__.py
@@ -18,6 +18,13 @@ def if_gt(float1, float2, float3, float4):
         return float4
 
 
+def poly_gt(float1, float2):
+    if float1 > float2:
+        return 1.
+    else:
+        return 3.
+
+
 def setup_toolbox(problem, args):
     # Set variables of our genetic program:
     variables = dict(
@@ -39,6 +46,7 @@ def setup_toolbox(problem, args):
     pset.addEphemeralConstant("clog", lambda: np.random.choice([2 ** i for i in range(-8, 9)]), ret_type=float)
 
     pset.addPrimitive(if_gt, [float, float, float, float], float)
+    pset.addPrimitive(poly_gt, [float, float], float)
     binary_operators = [operator.add, operator.mul, operator.sub, operator.truediv, operator.pow, max, min]
     unary_operators = [scipy.special.expit, operator.neg]
     for binary_operator in binary_operators:
@@ -50,11 +58,16 @@ def setup_toolbox(problem, args):
     def make_tuple(*args):
         return tuple([*args])
 
-    pset.addPrimitive(make_tuple, [float] * len(problem['hyperparameters']), typing.Tuple)
+    n_hyperparams = len(problem['hyperparameters'])
+    pset.addPrimitive(make_tuple, [float] * n_hyperparams, typing.Tuple)
 
     # More DEAP boilerplate...
     creator.create("FitnessMin", base.Fitness, weights=(1.0, -1.0))
-    creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
+
+    if args.phenotypic_plasticity:
+        creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin, plasticity=(True,) * n_hyperparams)
+    else:
+        creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
 
     toolbox = base.Toolbox()
     toolbox.register("expr", gp.genFull, pset=pset, min_=1, max_=3)
@@ -67,6 +80,6 @@ def setup_toolbox(problem, args):
     toolbox.register("mutate", random_mutation, pset=pset, max_depth=args.max_number_operators)
 
     toolbox.register("evaluate", functools.partial(try_evaluate_function,
-                                                   invalid=(1e-6,) * len(problem['hyperparameters'])))
+                                                   invalid=(1e-6,) * n_hyperparams))
 
     return toolbox, pset
