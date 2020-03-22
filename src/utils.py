028247657f6b7f2dcaffd6d3a0900699d709bc72
pfistfl <pfistererf@googlemail.com>
2020-03-22 10:46:20 +0100

    Fix full NaN bgu

--- src/utils.py
+++ src/utils.py
@@ -22,9 +22,14 @@ class SimpleImputerDuplicate(SimpleImputer):
 def simple_construct_pipeline_for_task(task, learner=None):
     cat_indices = task.get_dataset().get_features_by_type('nominal', [task.target_name])
     num_indices = task.get_dataset().get_features_by_type('numeric', [task.target_name])
+    
+    X, y = task.get_X_and_y()
+
+    all_nan = np.where([np.all(np.isnan(v)) for v in X.T])[0]
+    cat_indices = [i for i in cat_indices if not i in all_nan]
+    num_indices = [i for i in num_indices if not i in all_nan]
     n_cat, n_num = len(cat_indices), len(num_indices)
 
-    X, y = task.get_X_and_y()
     unique_values_cat_features = [list(sorted([v for v in set(values) if not np.isnan(v)]))
                                   for values in X[:, cat_indices].T]
 
@@ -32,7 +37,8 @@ def simple_construct_pipeline_for_task(task, learner=None):
                         ("impute", ColumnTransformer(
                             transformers=[
                                 ("nominal", SimpleImputer(strategy="most_frequent"), cat_indices),
-                                ("numeric", SimpleImputerDuplicate(strategy="mean"), num_indices)
+                                ("numeric", SimpleImputerDuplicate(strategy="mean"), num_indices),
+                                ("all_nan", SimpleImputerDuplicate(strategy="constant", fill_value = 0), all_nan)
                             ],
                             remainder="passthrough"
                         )),
