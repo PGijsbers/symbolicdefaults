04e443ec762f1b43eefb5901e2c0a5816f47945c
pfistfl <pfistererf@googlemail.com>
2020-05-07 17:42:18 +0200

    lrz script

--- cluster/submit_lrz.py
+++ cluster/submit_lrz.py
@@ -23,17 +23,17 @@ jobs=["mlr_svm", "mlr_glmnet"]
 
 for job in jobs:
     job_file = os.path.join(job_directory, f"{job}.job")
-    with open(job_file) as fh:
+    with open(job_file, 'w+') as fh:
         fh.writelines("#!/bin/bash\n")
         fh.writelines(f"#SBATCH --job-name={job}.job\n")
         fh.writelines(f"#SBATCH --output=.out/{job}.out\n")
         fh.writelines(f"#SBATCH --error=.out/{job}.err\n")
         fh.writelines(f"#SBATCH --time={hrs}-00:00\n")
-        fh.writelines(f"#SBATCH --mem={mem}\n" % mem)
-        fh.writelines(f"#SBATCH --clusters={cluster}")
-        fh.writelines(f"#SBATCH --mem_per_cpu={mem}\n" % mem)
-        fh.writelines(f"#SBATCH --clusters={cluster}")
+        fh.writelines(f"#SBATCH --mem={mem}\n")
+        fh.writelines(f"#SBATCH --clusters={cluster}\n")
+        fh.writelines(f"#SBATCH --mem_per_cpu={mem}\n")
+        fh.writelines(f"#SBATCH --clusters={cluster}\n")
         fh.writelines(f"#SBATCH --partition={partition}\n")
-        fh.writelines(f"python3 src/main.py {job} -o=runs/{job}_{suffix}.log")
+        fh.writelines(f"python3 src/main.py {job} -o=runs/{job}_{suffix}.log\n")
 
     os.system("sbatch %s" %job_file)
