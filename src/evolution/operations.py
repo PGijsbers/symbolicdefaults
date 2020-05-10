4b7af1a86c09a786d499aa35d7d1c14c2b1db563
pfistfl <pfistererf@googlemail.com>
2020-05-10 22:15:38 +0200

    add comments

--- src/evolution/operations.py
+++ src/evolution/operations.py
@@ -525,9 +525,13 @@ def mutTerminalReplacement(individual, pset):
     idxs = [(idx,ind) for idx, ind in enumerate(individual) if isinstance(ind, gp.Terminal)]
     # Sample a Terminal to be replaced
     idx, ind = random.sample(idxs, 1)[0]
+
     if random.random() > 0.5:
+        # replace a Terminal with same type Ephemeral
+        # Ephemerals contain the class constructor, not thhe class, are therefore of type 'type'
         individual[idx] = [t for t in pset.terminals[ind.ret] if isinstance(t, type)][0]()
     else:
+        # replace a Terminal with same type Symbolic
         valid = [t for t in pset.terminals[ind.ret] if isinstance(t, gp.Terminal)]
         if len(valid) > 0:
             individual[idx] = random.sample(valid, 1)[0]
