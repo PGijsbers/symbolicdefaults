86c92cee558641049977591e3ba3fde70e6e0d15
pfistfl <pfistererf@googlemail.com>
2020-05-13 01:26:04 +0200

    move

--- cluster/R/helpers.R
+++ cluster/R/helpers.R
@@ -64,3 +64,31 @@ get_task_ids = function(problem) {
   p = import_from_path("src.problem")
   p$Problem(problem)$valid_tasks
 }
+
+# Run an algorithm
+# @param algo :: algorithm name, e.g. classif.svm
+# @param task :: task_id, e.g. 3
+# @param str  :: tuple string, e.g. "make_tuple(1,1)"
+# @example
+# run_algo("mlr_svm", 3, "make_tuple(1,1)")
+run_algo = function(problem, task, str, parallel = 10L) {
+	  if (parallel)
+		      parallelMap::parallelStartMulticore(parallel, level = "mlr.resample")
+    on.exit(parallelMap::parallelStop())
+
+      lgr = get_logger("eval_logger")$set_threshold("info")
+      lgr$add_appender(lgr::AppenderFile$new("runs/mlr_evaluation_log.log"))
+        lgr$info(sprintf("Evaluating %s|%s|%s", problem, task, str))
+        lrn = make_preproc_pipeline(problem)
+	  hpars = eval_tuple(problem, task, str)
+	  setHyperPars(lrn, par.vals = parse_lgl(hpars))
+	    bmr = try({
+		        omltsk = getOMLTask(task)
+			    z = convertOMLTaskToMlr(omltsk, measures = mmce)
+			    benchmark(lrn, z$mlr.task, z$mlr.rin, measures = z$mlr.measures)
+			      })
+	    aggr = bmr$results[[1]][[1]]$aggr
+	      measure = "mmce.test.mean"
+	      lgr$info(sprintf("Result: %s: %s", measure, aggr[[measure]]))
+	        bmr$results[[1]][[1]]
+}
