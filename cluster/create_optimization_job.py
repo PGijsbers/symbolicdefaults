10014eb0f58d65729d17054c94d52f5cfa2a9c36
PGijsbers <p.gijsbers@tue.nl>
2020-05-12 23:41:05 +0200

    Update job script

--- cluster/create_optimization_job.py
+++ cluster/create_optimization_job.py
@@ -1,18 +1,47 @@
 import sys
+import itertools
 
-job_template = """\
+job_header = """\
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -t 24:00:00
+#SBATCH -t 36:00:00
 
-#cp -r ~/.openml/cache/ "$TMPDIR"/oml
+module load 2019
+module load Python/3.6.6-intel-2018b
 
-module load python/3.5.0
-python3 main.py {}
+cd ~/symbolicdefaults
+source venv/bin/activate
+
+"""
+
+job_footer = """\
+
+wait
 """
 
 if __name__ == '__main__':
-    job_name = sys.argv[1]
+    if len(sys.argv) > 1:
+        problems = [sys.argv[1]]
+    else:
+        problems = ['knn', 'svm', 'glmnet', 'rf', 'rpart', 'xgboost']
+    if len(sys.argv) > 2:
+        algorithms = [sys.argv[2]]  # random_search or mupluslambda
+    else:
+        algorithms = ["random_search", "mupluslambda"]
 
-    with open(job_name, 'w', newline='\n') as fh:
-        fh.write(job_template.format(' '.join(["'{}'".format(el) for el in sys.argv[2:]])))
+    start_command = "python src/main.py mlr_{problem} -o {log} -a {alg}"
+    for problem, algorithm in itertools.product(problems, algorithms):
+        job_name = f"jobs/{problem}_{algorithm}.job"
+        with open(job_name, 'a') as fh:
+            fh.write(job_header)
+
+        for i in range(10):
+            logfile = f"~/symbolicdefaults/runs/mlr_{problem}_{algorithm}_{i}.log"
+            cmd = start_command.format(problem=problem, log=logfile, alg=algorithm)
+            if algorithm == "random_search":
+                cmd += ' -mss 3'
+            with open(job_name, 'a') as fh:
+                fh.write(cmd + ' &\n')
+
+        with open(job_name, 'a') as fh:
+            fh.write(job_footer)
