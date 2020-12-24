7c3b8e1836df486a40904240229c4f3a1875cedb
PGijsbers <p.gijsbers@tue.nl>
2020-12-24 17:42:35 +0100

    add str2bool

--- cluster/create_jobs.py
+++ cluster/create_jobs.py
@@ -13,8 +13,6 @@ import itertools
 import os
 import sys
 
-from src.utils import str2bool
-
 sys.path.append("./src/")
 
 import pandas as pd
@@ -39,6 +37,17 @@ job_footer = """\
 
 wait
 """
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
 def cli_parser():
